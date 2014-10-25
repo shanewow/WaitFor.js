79c239881d51b501ae6f71cdfab6747ca5904945
shanewow <shanewow@users.noreply.github.com>
2014-10-24 20:53:56 -0400

    Adding main js file

---
+++ WaitFor.js
@@ -0,0 +1,44 @@
+/**
+ * @author Shane Kearney
+ *
+ */
+var WaitFor = {};
+
+WaitFor.initMethodArrayMap = {};
+WaitFor.existsString = "";
+
+WaitFor.these = function(classNames, initMethod){
+	classNames = classNames.replace(new RegExp(" ","g"),"");
+	if(WaitFor.exists(classNames)){
+		initMethod.call(this);
+	}else{
+		var initMethods = WaitFor.initMethodArrayMap[classNames] || [];
+		WaitFor.initMethodArrayMap[classNames] = initMethods.concat(initMethod);
+	}
+};
+
+WaitFor.ready = function(className){
+	if(WaitFor.debug)console.log("WaitFor.ready for "+className);
+	WaitFor.existsString = WaitFor.existsString.concat(className+",");
+	for(var classNames in WaitFor.initMethodArrayMap){
+		if(WaitFor.exists(classNames)){
+			var initMethods = WaitFor.initMethodArrayMap[classNames];
+			delete WaitFor.initMethodArrayMap[classNames];
+			if(!!initMethods){
+				for(var i = 0; i < initMethods.length; i++){
+					initMethods[i].call(this);
+				}
+			}
+		}
+	}
+};
+
+WaitFor.exists = function(classNames){
+	var names = classNames.split(",");
+	for(var i = 0; i < names.length; i++){
+		if(WaitFor.existsString.indexOf(names[i]+",") == -1){
+			return false;
+		}
+	}
+	return true;
+};
